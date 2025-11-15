c589bb552571f39debd90d85f3d1424b5b582f8b
Mihifidem <mihifidem@gmail.com>
2025-11-15 13:27:15 +0100

    feat: register Coche API routes using DRF router

--- coches/urls.py
+++ coches/urls.py
@@ -1,7 +1,14 @@
+from django.contrib import admin
 from django.urls import path, include
-from . import views
+from rest_framework import routers
+from coches.views import CocheViewSet
 
+
+router = routers.DefaultRouter()
+router.register('coches', CocheViewSet)
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('' includes(views.home))
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
