26b82417a7a1f2f71b81bb09da06bcbbf1395aa0
Mihifidem <mihifidem@gmail.com>
2025-11-15 13:21:27 +0100

    feat: add basic Coche url and project url

--- coches/models.py
+++ coches/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Coche(models.Model):
+    modelo = models.CharField(max_length=100)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.modelo} - {self.precio}€"
+
--- coches/views.py
+++ coches/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Bienvenido a la API de Coches de Segunda Mano 🚗</h1>")
