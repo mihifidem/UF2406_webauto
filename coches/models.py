c95e01524d97c8117e9cc840faec2164348a7579
Mihifidem <mihifidem@gmail.com>
2025-11-15 13:22:49 +0100

    feat: add basic Coche model for initial development with sqlite

--- coches/models.py
+++ coches/models.py
@@ -1,6 +1,4 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 
 class Coche(models.Model):
@@ -9,4 +7,3 @@ class Coche(models.Model):
 
     def __str__(self):
         return f"{self.modelo} - {self.precio}€"
-
