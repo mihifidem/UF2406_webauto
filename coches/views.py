0a8345b564c5b1def0aea5b2b62da25132d59e42
Mihifidem <mihifidem@gmail.com>
2025-11-15 13:24:35 +0100

    feat: create CocheViewSet with CRUD operations

--- coches/views.py
+++ coches/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Coche
+from .serializers import CocheSerializer
+
+class CocheViewSet(viewsets.ModelViewSet):
+    queryset = Coche.objects.all()
+    serializer_class = CocheSerializer
 
 def home(request):
     return HttpResponse("<h1>Bienvenido a la API de Coches de Segunda Mano 🚗</h1>")
